fec1d726509bfabda08963819106792584e925d4
Elliott Palmer <litepalmer@gmail.com>
2018-03-20 15:58:52 +0000

    Added code to ensure text file names are kept when reading in data

--- judithBoxplot.R
+++ judithBoxplot.R
@@ -6,18 +6,18 @@ library(purrr)
 library(data.table)
 library(ggplot2)
 
-
+#Read in 
+files <- list.files("/Users/elliottpalmer/Downloads/judith")
 data <- lapply(files, FUN=read.table, header=TRUE)
+names(data) <- basename(files)
 df <- rbindlist(data, idcol = "index")
 
- 
-df <- read.delim("/Users/elliottpalmer/Downloads/Galaxy_Blank_Filter_Peak_Intensity_Matrix_Dust1_Apolar_Pos_10.txt",sep = "\t")
-df.melt <- melt(data = df, id.vars = "mz")
+df.melt <- melt(data = df, id.vars = c("mz","index") )
 df.rsd <- filter(df.melt, variable == "rsd", value != 0)
 df.outOf <- filter(df.melt, variable == "present")
 
-bp <- ggplot(df.rsd, aes(x=1, y=value)) + geom_boxplot()
+bp <- ggplot(df.rsd, aes(x=index, y=value, group = index)) + geom_boxplot()
 bp 
 
-outOf <- ggplot(df.outOf, aes(value)) + geom_bar()
+outOf <- ggplot(df.outOf, aes(value)) + geom_bar() + facet_grid(. ~ index)
 outOf
