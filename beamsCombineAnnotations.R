acb19a6790719444d5a09e787ac18e623cd038a4
Elliott Palmer <litepalmer@gmail.com>
2018-04-17 13:22:16 +0100

    added code to combine with xcms output file

--- beamsCombineAnnotations.R
+++ beamsCombineAnnotations.R
@@ -1,5 +1,10 @@
 library(plyr)
 
-data <- read.delim("/Users/elliottpalmer/Downloads/summary.txt")
-combinedAnnotations <- ddply(data, .(name), summarize,
+beamsSummary <- read.delim("/Users/elliottpalmer/Downloads/summary.txt")
+combinedAnnotations <- ddply(beamsSummary, .(name), summarize,
               compound_name=paste(unique(compound_name),collapse=","))
+
+xcmsOutput <- read.csv("/Users/elliottpalmer/Downloads/final_Lipid_Pos_2_1dil1FILTERED.csv")
+xcmsOutput$name <- paste("M", xcmsOutput$X, sep='') 
+
+mergedData <- merge(xcmsOutput, combinedAnnotations, by="name")
